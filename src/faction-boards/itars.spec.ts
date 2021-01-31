365ef6f101ebab038fde8578d3782ae35aa15b27
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2021-01-31 18:14:33 +0100

    use const

--- src/faction-boards/itars.spec.ts
+++ src/faction-boards/itars.spec.ts
@@ -38,7 +38,7 @@ describe('Itars', () => {
   });
 
   it("should autocharge when no power tokens are going to be put in area3", () => {
-    let engine = new Engine();
+    const engine = new Engine();
     expect(engine.autoChargeItars(2, 2)).to.be.true;
     expect(engine.autoChargeItars(1, 2)).to.be.false;
     expect(engine.autoChargeItars(0, 1)).to.be.false;
